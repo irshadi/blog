2d5470add40859b66726e06e86e3b4e93636ac5b
irshadi <irshadi@delman.io>
2020-09-12 23:16:54 +0700

    Congfigure Tailwind & PostCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { HomeView } from "../views/Home";
-import "../css/index.css";
 
 const HomePage = () => {
   return <HomeView />;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "../../css/index.css";
 
 export const HomeView = () => {
-  return <div className="bg-black">My Home View</div>;
+  return <div className="bg-pink-500">My Blog</div>;
 };
