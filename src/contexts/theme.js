9a32d3c842456a44940c7649ca17f4602540fb49
irshadi <bagasputro.irshadi@gmail.com>
2020-09-13 16:06:14 +0700

    Create Local Storage Constant

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { STORAGE_VALUE } from "../constants/storageValue";
 import { THEME_MODE } from "../constants/theme";
 import { useThemeMode } from "../hooks/useThemeMode";
 
@@ -6,7 +7,8 @@ const ThemeContext = React.createContext({});
 
 // eslint-disable-next-line react/prop-types
 const ThemeContextProvider = ({ children }) => {
-  const defaultThemeValue = localStorage.getItem("theme") || THEME_MODE.LIGHT;
+  const defaultThemeValue =
+    localStorage.getItem(STORAGE_VALUE.THEME) || THEME_MODE.LIGHT;
   const value = useThemeMode({ defaultTheme: defaultThemeValue });
   return (
     <ThemeContext.Provider value={{ ...value }}>
--- src/hooks/useThemeMode.js
+++ src/hooks/useThemeMode.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { STORAGE_VALUE } from "../constants/storageValue";
 
 export const useThemeMode = ({ defaultTheme }) => {
   const [theme, _setTheme] = React.useState(defaultTheme);
 
   const setTheme = preferredTheme => {
     _setTheme(preferredTheme);
-    localStorage.setItem("theme", theme);
+    localStorage.setItem(STORAGE_VALUE.THEME, theme);
   };
 
   React.useEffect(() => {
-    localStorage.setItem("theme", theme);
+    localStorage.setItem(STORAGE_VALUE.THEME, theme);
   }, [defaultTheme]);
 
   return {
