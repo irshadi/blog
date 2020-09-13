94e49e987ef82e6a96acc88475aa0452c5a0839a
irshadi <irshadi@delman.io>
2020-09-13 14:32:58 +0700

    Create Dummy Text for Landing Page

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import "../../css/index.css";
 
 export const HomeView = () => {
-  return <div className="bg-pink-500">My Blog</div>;
+  return (
+    <div className="flex flex-col">
+      <section className="bg-gradient-to-from-gray-200 from-white">
+        <span className="text-4xl font-semibold leading-none">
+          Hello,
+          <br />
+          Most of the time it's just gibberish.
+        </span>
+      </section>
+    </div>
+  );
 };
