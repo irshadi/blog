eb9c1954911e505d3eef9ee7dedf82a9763d3a13
irshadi <bagasputro.irshadi@gmail.com>
2020-09-13 15:13:28 +0700

    Add react-icons

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
+import { IoMdMoon } from "react-icons/io";
 
 export const NavigationBar = () => {
   return (
-    <nav className="bg-red-100 w-full pt-12 flex">
+    <nav className="w-full pt-12 flex justify-between">
       <Link to="/" replace className="text-xl font-semibold">
         irshadi.id
       </Link>
-      <div></div>
+      <div className="flex justify-end w-3/12 px-2">
+        <IoMdMoon className="text-2xl font-semibold" />
+      </div>
     </nav>
   );
 };
