c1a89204e6376f98a810337e6b16c9ecfa971563
= <=>
2020-09-16 00:24:25 +0700

    Disable Comment

--- src/components/PostComponent/index.js
+++ src/components/PostComponent/index.js
@@ -6,10 +6,10 @@ const POST_TYPE = {
 };
 
 export const Post = ({ rows, tiles }) => {
-  const POST_COMPONENT_MAP = {
-    [POST_TYPE.ROWS]: () => {},
-    [POST_TYPE.TILES]: () => {}
-  };
+  // const POST_COMPONENT_MAP = {
+  //   [POST_TYPE.ROWS]: () => {},
+  //   [POST_TYPE.TILES]: () => {}
+  // };
   // const PostsComponent = POST_COMPONENT_MAP  
   return <div className="text-primary font-semibold">Post Component</div>;
 };
