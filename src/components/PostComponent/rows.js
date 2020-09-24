186379c5231ea426b18004d6a2ecdce660e8ca49
= <=>
2020-09-24 23:52:04 +0700

    Finish Make Row Component

--- src/components/PostComponent/rows.js
+++ src/components/PostComponent/rows.js
@@ -3,13 +3,15 @@ import React from "react";
 
 export const RowPost = ({ title, content, img, metadata, createdAt }) => {
   return (
-    <div className="flex flex-col w-1/3">
-      <img src={img} />
-      <span className="text-primary font-semibold">{title}</span>
-      <span className="text-secondary pt-4">{content}</span>
-      <div className="flex justify-between pt-4">
-        <span className="text-secondary">{createdAt.toDateString()}</span>
-        <span className="text-secondary font-semibold">{metadata}</span>
+    <div className="flex flex-col bg-card w-1/3 rounded-lg shadow-xl opacity-50 hover:opacity-100 duration-200">
+      <img className="rounded-t-lg rounded-b-none overflow-hidden" src={img} />
+      <div className="p-6 flex flex-col">
+        <span className="text-primary font-semibold text-xl">{title}</span>
+        <span className="text-secondary pt-2">{content}</span>
+        <div className="flex justify-between pt-4">
+          <span className="text-secondary">{createdAt.toDateString()}</span>
+          <span className="text-secondary font-semibold">{metadata}</span>
+        </div>
       </div>
     </div>
   );
