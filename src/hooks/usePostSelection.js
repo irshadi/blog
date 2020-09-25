993ca196631ea075be7e63d8d602c48fc7964597
irshadi <bagasputro.irshadi@gmail.com>
2020-09-25 16:00:21 +0700

    Modify usePostSelection to Have PageConfig

--- src/hooks/usePostSelection.js
+++ src/hooks/usePostSelection.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { POST_MODE } from "../constants/postMode";
 
+const defaultPageConfig = {
+  currentPage: 1,
+  hasPreviousPage: false,
+  hasNextPage: false
+};
+
 export const usePostSelection = () => {
   const [postMode, setPostMode] = React.useState(POST_MODE.ROWS);
+  const [pageConfig, setPageConfig] = React.useState(defaultPageConfig);
 
   return {
     postMode,
-    setPostMode
+    setPostMode,
+    pageConfig,
+    setPageConfig
   };
 };
