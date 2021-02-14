71fab6e2ddbc45d3ad3050fc0f67391d5effbe40
irshadi <irshadi@delman.io>
2021-02-14 13:53:21 +0700

    fix: ui on codeblock component

--- src/components/MDXRenderer/Code/index.js
+++ src/components/MDXRenderer/Code/index.js
@@ -1,20 +1,51 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import { useClipboardCopy } from "../../../hooks/useClipboardCopy";
+import { MdContentCopy } from "react-icons/md";
 
-const CopyButton = props => {
+const CopyButton = ({ isCopied, ...props }) => {
   return (
-    <Button colorScheme="teal" variant="ghost" {...props}>
-      Copy
+    <Button
+      positon="absolute"
+      colorScheme="gray"
+      size="md"
+      variant="solid"
+      {...props}
+      float="right"
+      // top="1em"
+      // right="1em"
+      color="gray.700"
+    >
+      <MdContentCopy />
+      <Text px="0.5em">{isCopied ? "Copied" : "Copy"}</Text>
     </Button>
   );
 };
 
 export const CodeBlock = ({ codeString, language }) => {
+  const [isCopied, setIsCopied] = React.useState(false);
   const { copyToClipboard } = useClipboardCopy();
-  const handleClickCopy = () => copyToClipboard(codeString);
+
+  const handleClickCopy = () => {
+    if (isCopied) {
+      return;
+    }
+    copyToClipboard(codeString);
+    setIsCopied(!isCopied);
+  };
+
+  // Reset Button State
+  React.useEffect(() => {
+    if (!isCopied) {
+      return;
+    }
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  }, [isCopied, setIsCopied]);
 
   return (
     <Highlight
@@ -25,15 +56,26 @@ export const CodeBlock = ({ codeString, language }) => {
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => {
         return (
-          <pre className={className} style={{ ...style }}>
-            <CopyButton onClick={handleClickCopy} />
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+          <pre
+            className={className}
+            style={{ ...style, borderRadius: ".25em", padding: "1.5em" }}
+          >
+            <CopyButton isCopied={isCopied} onClick={handleClickCopy} />
+            {tokens.map((line, i) => {
+              return (
+                <div
+                  {...getLineProps({ line, key: i })}
+                  style={{ display: "flex", fontSize: "0.85em" }}
+                >
+                  <Text pr="1.5em" color="gray.600">
+                    {i + 1}
+                  </Text>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              );
+            })}
           </pre>
         );
       }}
