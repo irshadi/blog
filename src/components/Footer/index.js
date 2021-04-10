0bc129d699a654c1ef6d7463acd3dab220c7870b
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2021-04-10 13:56:13 +0700

    Bugfix/20 footer issue
    style: bugfix on the footer and assign link (#31)

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,31 +2,39 @@ import React from "react";
 import { SiBehance, SiLinkedin, SiGithub, SiTwitter } from "react-icons/si";
 import { Divider, Flex, Heading } from "@chakra-ui/react";
 import { IconButton } from "../Button";
+import { navigate } from "@reach/router";
+
+const externalSocialMediaLinks = [
+  { name: "GitHub", icon: SiGithub, link: "https://github.com/irshadi" },
+  { name: "Behance", icon: SiBehance, link: "https://www.behance.net/irshadi" },
+  { name: "Twitter", icon: SiTwitter, link: "https://twitter.com/irshadi" },
+  {
+    name: "LinkedIn",
+    icon: SiLinkedin,
+    link: "https://www.linkedin.com/in/irshadi/"
+  }
+];
 
 export const Footer = () => {
   return (
-    <Flex as="footer" justifyContent="space-between" alignItems="center">
-      <Heading fontSize="1em" fontWeight="semibold">
+    <Flex
+      w="100%"
+      as="footer"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Heading w="15%" fontSize="1em" fontWeight="semibold">
         © {new Date().getFullYear()} irshadi.id
       </Heading>
-      <Divider />
-      <Flex>
-        <IconButton
-          Icon={SiGithub}
-          onClick={() => window.open("https://github.com/irshadi", "_blank")}
-        />
-        <IconButton
-          Icon={SiBehance}
-          onClick={() => window.open("https://github.com/irshadi", "_blank")}
-        />
-        <IconButton
-          Icon={SiTwitter}
-          onClick={() => window.open("https://github.com/irshadi", "_blank")}
-        />
-        <IconButton
-          Icon={SiLinkedin}
-          onClick={() => window.open("https://github.com/irshadi", "_blank")}
-        />
+      <Divider w="70%" />
+      <Flex w="15%" justify="flex-end">
+        {externalSocialMediaLinks.map(({ name, icon, link }, id) => (
+          <IconButton
+            key={`${id}-${name}`}
+            Icon={icon}
+            onClick={() => navigate(link)}
+          />
+        ))}
       </Flex>
     </Flex>
   );
