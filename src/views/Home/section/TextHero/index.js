6f68420780a2de90c87a83a4c4df9e90aac93d63
irshadi <bagasputro.irshadi@gmail.com>
2021-04-12 02:25:33 +0700

    style: implement responsive on mobile

--- src/views/Home/section/TextHero/index.js
+++ src/views/Home/section/TextHero/index.js
@@ -3,7 +3,7 @@ import { Box, Heading } from "@chakra-ui/react";
 
 export const TextHero = () => {
   return (
-    <Box fontSize="2.5em" fontWeight="semibold">
+    <Box my={["1em"]} fontSize="2.5em" fontWeight="semibold">
       <Heading>Hello,</Heading>
       <Heading>Most of the time it's just gibberish.</Heading>
     </Box>
