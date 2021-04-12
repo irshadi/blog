3d5f373c4eef94f29994ad34f042c975e8b5278a
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2021-04-12 17:08:02 +0700

    Feature/39 responsive issue in code renderer (#43)
    * style: fix prism rerender responsive mode
    
    * style: fix responsive mode

--- src/components/MDXRenderer/Code/index.js
+++ src/components/MDXRenderer/Code/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
-import { Button, Text, useColorModeValue } from "@chakra-ui/react";
+import { Button, Text, Flex, Box, useColorModeValue } from "@chakra-ui/react";
 import { useClipboardCopy } from "../../../hooks/useClipboardCopy";
 import { MdContentCopy } from "react-icons/md";
 
 const CopyButton = ({ isCopied, ...props }) => {
   const color = useColorModeValue("gray.700", "gray.200");
+  const bgColor = useColorModeValue("gray.200", "gray.700");
   return (
     <Button
       positon="absolute"
       colorScheme="gray"
       size="md"
       variant="solid"
-      {...props}
       float="right"
       color={color}
+      bg={bgColor}
+      {...props}
     >
       <MdContentCopy />
       <Text px="0.5em">{isCopied ? "Copied" : "Copy"}</Text>
@@ -55,16 +57,21 @@ export const CodeBlock = ({ codeString, language }) => {
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => {
         return (
-          <pre
+          <Box
+            as={"pre"}
             className={className}
-            style={{ ...style, borderRadius: ".25em", padding: "1.5em" }}
+            rounded=".25em"
+            px="1.5em"
+            py="1em"
+            style={{ ...style }}
+            overflowX="scroll"
           >
             <CopyButton isCopied={isCopied} onClick={handleClickCopy} />
             {tokens.map((line, i) => {
               return (
-                <div
+                <Flex
                   {...getLineProps({ line, key: i })}
-                  style={{ display: "flex", fontSize: "0.85em" }}
+                  fontSize={[".5em", ".85em"]}
                 >
                   <Text pr="1.5em" color="gray.600">
                     {i + 1}
@@ -72,10 +79,10 @@ export const CodeBlock = ({ codeString, language }) => {
                   {line.map((token, key) => (
                     <span {...getTokenProps({ token, key })} />
                   ))}
-                </div>
+                </Flex>
               );
             })}
-          </pre>
+          </Box>
         );
       }}
     </Highlight>
