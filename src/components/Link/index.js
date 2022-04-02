230379d05a9bfc77af1a99e6197b72340162e834
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2022-04-02 16:50:45 +0700

    feat: create link wrapper

---
+++ src/components/Link/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link as GatsbyLink } from "gatsby";
+import { Link, Button, Text, useColorModeValue } from "@chakra-ui/react";
+
+export const LinkWrapper = ({ children, ...props }) => {
+  const color = useColorModeValue("gray.700", "gray.100");
+  const hasChild = React.isValidElement(children);
+  const { title, to } = props;
+
+  console.log(hasChild, "<<< hasChild");
+
+  if (!hasChild) {
+    return (
+      <Link
+        as={GatsbyLink}
+        to={to}
+        replace
+        _hover={{ border: "none" }}
+        {...props}
+      >
+        <Button variant="link" p="inherit" w="100%">
+          <Text
+            textTransform="lowercase"
+            fontWeight="extrabold"
+            color={color}
+            letterSpacing="tight"
+          >
+            {title}
+          </Text>
+        </Button>
+      </Link>
+    );
+  }
+
+  return (
+    <Link
+      as={GatsbyLink}
+      to={to}
+      replace
+      _hover={{ border: "none" }}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+};
