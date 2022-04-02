c504264c27d42042fb7eae16aa3f99ced597984f
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2022-04-03 03:02:14 +0700

    style: fix post mode component

--- src/views/Home/section/PostSortHeader/index.js
+++ src/views/Home/section/PostSortHeader/index.js
@@ -1,31 +1,26 @@
 import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
 import { AuthorCard } from "../../../../components/AuthorCard";
 import { SortSelections } from "../../../../components/SortSelections";
 
 export const PostSortHeader = () => {
   return (
-    <Flex w="100%" flexDir={["column", "row"]} justify="space-between" py="2em">
+    <Box w="100%" py="2em">
       <Flex w={["100%", "75%"]}>
         <AuthorCard />
       </Flex>
       <Flex
-        w={["100%", "25%"]}
-        mt={["2em", 0]}
+        w="100%"
+        mt={["2em"]}
+        justify="space-between"
         align={["center"]}
         display={["none", "flex"]}
       >
-        <Text
-          w={"50%"}
-          visibility={["visible", "hidden"]}
-          opacity={"75%"}
-          fontSize="small"
-          fontWeight="semibold"
-        >
+        <Text w={"50%"} opacity={"75%"} fontSize="small" fontWeight="semibold">
           View Mode
         </Text>
         <SortSelections />
       </Flex>
-    </Flex>
+    </Box>
   );
 };
