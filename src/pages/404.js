d81c3ec9bccf966f7944aa05007094ad98a3d018
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2022-04-03 17:30:44 +0700

    feat: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FourOFour } from "../views/404";
+import { PageWrapper } from "../components/PageWrapper";
 
-const FoF = () => <FourOFour />;
+const FoF = () => <PageWrapper Views={FourOFour} />;
 
 export default FoF;
