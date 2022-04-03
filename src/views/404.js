b7f6b53136a0b0d34110221543ae13f496aeecae
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2022-04-03 17:34:07 +0700

    Housekeeping/60 clean up code (#63)
    * rm: usetheme mode
    
    * chore: rm uneeded import
    
    * feat: 404 page
    
    * rm: console

--- src/views/404.js
+++ src/views/404.js
@@ -1,7 +1,9 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 export const FourOFour = () => (
-  <div className="text-primary flex items-center justify-center">
-    Page not Found
-  </div>
+  <Flex w="100%" h={["30em"]} flexDir="column" align="center" justify="center">
+    <Heading fontSize="10em">404</Heading>
+    <Text>Page not Found</Text>
+  </Flex>
 );
