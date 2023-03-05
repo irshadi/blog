93f39f96c66b49aaf3e05fb954447975186f8c81
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2023-03-05 15:25:05 +0700

    feat: create constants

---
+++ src/constants/postMode.js
@@ -0,0 +1,4 @@
+export const POST_MODE_MAP = {
+  ROWS: "ROWS",
+  TILES: "TILES"
+};
---
+++ src/constants/storageValue.js
@@ -0,0 +1,4 @@
+export const STORAGE_VALUE_MAP = {
+  COLOR_MODE: "chakra-ui-color-mode",
+  POST_MODE: "irshadi-blog-post-mode"
+};
